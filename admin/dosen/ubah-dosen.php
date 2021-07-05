ab5c1744931cdb5fd1a973ec397416face4865e9
Yoga Adi Nugroho <78912216+yogadinugroho@users.noreply.github.com>
2021-07-05 20:32:01 +0700

    menambahkan syntax php
    menambahkan syntax php agar fitur ubah data bisa berfungsi dengan baik

--- admin/dosen/ubah-dosen.php
+++ admin/dosen/ubah-dosen.php
@@ -1,3 +1,45 @@
+<?php 
+
+session_start();
+
+require 'functions.php';
+
+if(!isset($_SESSION["login"])){
+    header("Location: ../../index.php");
+    exit;
+}
+
+// ambil data di URL 
+$nip = $_GET["nip"];
+
+$dosen = query(" SELECT * FROM tabel_biodata_dosen WHERE nip = $nip")[0];
+
+if( isset($_POST["submit"]) ) {
+
+    // cek apakah data berhasil diubah atau tidak
+    // menggunakn funcion ubah()
+    
+    if( ubah($_POST) > 0 ) {
+        echo "
+            <script>
+                alert('data berhasil diubah!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    } else {
+        echo "
+            <script>
+            alert('data gagal diubah!');
+            document.location.href = 'index.php';
+            </script>
+        ";
+    }
+
+}
+
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">

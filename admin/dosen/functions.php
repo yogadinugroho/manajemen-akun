7d9e3ca6456dc1ffd9c7075f0feca29d8c1f631d
Yoga Adi Nugroho <78912216+yogadinugroho@users.noreply.github.com>
2021-07-05 20:06:01 +0700

    inisialisasi file functions.php
    inisialisasi config dan function query

---
+++ admin/dosen/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "2197101034");
+
+function query($query) {
+    global $conn;
+    // setelah terkoneksi dengan database, maka ambil isinya, disini masih berupa tabel
+    $result = mysqli_query($conn, $query);
+    // mempersiapkan wadah barang dari lemari yang diambil, bukan lagi tabelnya
+    $rows = [];
+
+    while( $row = mysqli_fetch_assoc($result) ) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+?

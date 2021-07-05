118441db5b06619e315bd9c5d28efd925a7c68b4
Yoga Adi Nugroho <78912216+yogadinugroho@users.noreply.github.com>
2021-07-05 20:15:28 +0700

    menambahkan function tambah dan upload

--- admin/dosen/functions.php
+++ admin/dosen/functions.php
@@ -21,5 +21,83 @@ function hapus($nip) {
     return mysqli_affected_rows($conn);
 }
 
+function tambah($data) {
+    global $conn;
+    
+    // upload gambar
+    $gambar = upload();
+    if ( !$gambar ) {
+        return false;
+    }
+    
+    // ambil data dari tiap elemen dalam form
+    $nip = htmlspecialchars($data["nip"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data["email"]);
+
+    // query untuk memasukkan data ke database
+    $query = "INSERT INTO tabel_biodata_dosen VALUES 
+            ('$nip', '$nama', '$email', '$gambar')";
+
+    mysqli_query($conn, $query);    
+
+    return mysqli_affected_rows($conn);
+}
+
+function upload() {
+    
+    // ambil dahulu isi dari $_FILES  
+
+    $namaFile = $_FILES['gambar']['name'];
+    $ukuranFile = $_FILES['gambar']['size'];
+    $error = $_FILES['gambar']['error'];
+    $tmpName = $_FILES['gambar']['tmp_name'];
+
+
+    // cek apakah tidak ada gambar yang diupload
+    if( $error === 4  ) {
+        echo "
+        <script>
+            alert('Silahkan Pilih Gambar Terlebih Dahulu');
+        </script>";
+
+        return false;
+    }
+
+    // cek apakah yang diupload adalah gambar
+    $ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
+    $ekstensiGambar = explode('.', $namaFile);
+    $ekstensiGambar = strtolower(end($ekstensiGambar));
+    if( !in_array($ekstensiGambar, $ekstensiGambarValid) ){
+        echo "
+        <script>
+            alert('Yang diupload bukan gambar!');
+        </script>";
+
+        return false;
+    }
+
+
+    // cek jika gambar ukurannya besar
+    if( $ukuranFile > 1000000 ) {
+        echo "
+        <script>
+            alert('Ukuran file terlalu besar');
+        </script>";
+
+        return false;
+    }
+
+    // generate nama file baru
+    $namaFileBaru = uniqid();
+    $namaFileBaru .= '.';
+    $namaFileBaru .= $ekstensiGambar;
+
+    // lokasi relatif terhadap file ini
+    move_uploaded_file($tmpName, 'img/' . $namaFileBaru);
+
+    return $namaFileBaru;
+
+
 
 ?

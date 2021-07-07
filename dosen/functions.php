df1d8b4733884653946d50e7a79b22703b93e6d9
Yoga Adi Nugroho <78912216+yogadinugroho@users.noreply.github.com>
2021-07-07 21:03:31 +0700

    inisalisasi file functions
    menambahkan session dan function query

---
+++ dosen/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+if(!isset($_SESSION["login"])){
+    header("Location: ../index.php");
+    exit;
+}
+
+$conn = mysqli_connect("localhost". "root", "", "2197101034");
+
+function query($query) {
+    global $conn;
+    // setelah terkoneksi dengan database, maka ambil isinya, disini masih berupa tabel
+    $result = mysqli_query($conn, $query);
+    // mempersiapkan wadah barang dari lemari yang diambil, bukan lagi tabelnya
+    $rows = [];
+
+    while( $row = mysqli_fetch_assoc($result) ) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+?>
